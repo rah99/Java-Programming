553875bd8c7b70b867fb582ef6bb646786e3c318
HIL18001579 <62950715+HIL18001579@users.noreply.github.com>
2020-10-12 23:31:56 +0100

    Update ThreadsMain.java

--- threadArrays/ThreadsMain.java
+++ threadArrays/ThreadsMain.java
@@ -1,10 +1,27 @@
 package threadArrays;
 
+import java.util.Scanner;
+
 public class ThreadsMain {
 	
 	private static int arrNum = 10;
 
 	public static void main(String[] args) throws InterruptedException {
+		int num = 0;
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Enter how many threads you would like in the array: ");
+		while (!scanner.hasNextInt()) {
+			scanner.nextLine();
+			System.out.println("Whole numbers only, please reenter your number of threads: ");
+		}
+		num = scanner.nextInt();
+		arrNum = num;
+		scanner.nextLine();
+		System.out.println("You entered: " + num);
+		System.out.println("Press enter to continue");
+		scanner.nextLine();
+		
 		Runnable hwodyObj = new ThreadRunnable();
 
 		Thread []thread = new Thread[arrNum];
@@ -15,6 +32,8 @@ public class ThreadsMain {
 		for (int i = 0; i < arrNum; i++) {
 			thread[i].join();
 		}
+		
+		scanner.close();
 	}
 }
 // https://www.youtube.com/watch?v=nuMHF6M_FdA
