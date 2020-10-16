b8c497967798c39c6a465f1d26448faa9002a30e
HIL18001579 <62950715+HIL18001579@users.noreply.github.com>
2020-10-16 15:11:00 +0100

    Update Deadlock.java

--- classwork_lessonDeadLock/Deadlock.java
+++ classwork_lessonDeadLock/Deadlock.java
@@ -9,10 +9,10 @@ public class Deadlock {
 			synchronized (lock) {
 				while (counter > 1) {
 					lock.wait();
-					System.out.println("Waiting in method 1");
+					System.out.println("Waiting in method 1 " + "counter = " + counter);
 				}
 				counter++;
-				System.out.println("Increase M1");
+				System.out.println("Increase M1 " + "counter = " + counter);
 				lock.notify();		
 			}
 		}
@@ -23,10 +23,10 @@ public class Deadlock {
 			synchronized (lock) {
 				while (counter > 2) {
 					lock.wait();
-					System.out.println("Waiting in method 2");
+					System.out.println("Waiting in method 2 " + "counter = " + counter);
 				}
 				counter++;
-				System.out.println("Increase M2");
+				System.out.println("Increase M2 " + "counter = " + counter);
 				lock.notifyAll(); // Without notifyAll() the threads deadlock - only need one notifyAll from the thread that is still alive
 			}
 		}
@@ -37,10 +37,10 @@ public class Deadlock {
 			synchronized (lock) {
 				while (counter < 3) {
 					lock.wait();
-					System.out.println("Waiting in method 3");
+					System.out.println("Waiting in method 3 " + "counter = " + counter);
 				}
 				counter = 0;
-				System.out.println("Zero");
+				System.out.println("Zero " + "counter = " + counter);
 				lock.notify();
 			}
 		}
