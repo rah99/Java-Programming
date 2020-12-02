0f57b4e1041f8bfc496abbaf038816ccee2f8014
HIL18001579 <62950715+HIL18001579@users.noreply.github.com>
2020-12-02 14:32:11 +0000

    Updates.

--- overloadingMonitorLock/CleanHair.java
+++ overloadingMonitorLock/CleanHair.java
@@ -1,16 +1,26 @@
 package overloadingMonitorLock;
 
+import java.time.Duration;
+import java.time.LocalTime;
+
 public class CleanHair {
 	String currentState = ""; // The object state
+	LocalTime start; // These variables are only to display that the time between execution is per the brief i.e. 2 seconds
+	LocalTime end; // src = https://www.baeldung.com/java-period-duration
+	long duration;
 
 	synchronized void Lather() {
 		currentState = "Lather";
 		System.out.println(currentState + " " + Thread.currentThread().getId());
-		notify();
+		notify(); // once the thread has printed the above it will send a notification that it has completed it's task so as any other thread waiting knows to activate - code below in the try/catch block then tells this thread to wait
 		while (currentState.equals("Lather")) {
 			try {
-				Thread.sleep(2000);
-				wait();	
+				start = LocalTime.now(); // setting variable "start" value
+				Thread.sleep(2000); // tells the thread how long to be inactive
+				end = LocalTime.now(); // setting variable "end" value
+				duration = Duration.between(start, end).getSeconds(); // setting variable "duration" value
+				System.out.println(duration + " seconds waited."); // printing the duration of the wait period to verify the thread waits as per the brief - would be omitted if in actual use
+				wait(); // thread will wait until it is notified
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
@@ -23,7 +33,11 @@ public class CleanHair {
 		notify();
 		while (currentState.equals("Rinse")) {
 			try {
+				start = LocalTime.now();
 				Thread.sleep(2000);
+				end = LocalTime.now();
+				duration = Duration.between(start, end).getSeconds();
+				System.out.println(duration + " seconds waited.");
 				wait();
 			} catch (InterruptedException e) {
 				e.printStackTrace();
--- overloadingMonitorLock/WaitNotifyExecServe.java
+++ overloadingMonitorLock/WaitNotifyExecServe.java
@@ -9,7 +9,10 @@ public class WaitNotifyExecServe {
 		ExecutorService executors = Executors.newFixedThreadPool(2);
 		CleanHair clean = new CleanHair();
 
-		executors.execute(new HairThreadRunnable("Lather", clean));
-		executors.execute(new HairThreadRunnable("Rinse", clean));
+//		executors.execute(new HairThreadRunnable("Lather", clean)); //execute simply runs the task and can only be used for a runnable class
+//		executors.execute(new HairThreadRunnable("Rinse", clean));
+		
+		executors.submit(new HairThreadRunnable("Lather", clean)); // submit runs the task and can be used to return a Future object for both runnable and callable classes
+		executors.submit(new HairThreadRunnable("Rinse", clean));
 	}
 }
